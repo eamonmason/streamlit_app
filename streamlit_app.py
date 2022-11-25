86ce50643b3e9f7b96023ebdea3ce3818ab0e87f
Eamon Mason <eamon.mason@thomsonreuters.com>
2022-11-25 14:14:16 +0000

    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('First streamlit app')
 streamlit.header('Gonna do something')
 streamlit.text('test1')
 streamlit.text('test2')
 streamlit.text('test3')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
